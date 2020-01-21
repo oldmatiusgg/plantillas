744e674f60bc8e978181b8746238b5b837d385f7
matiusgg <newpowerexperience2016@gmail.com>
2020-01-21 15:17:44 +0100

    Se ha agregado la logica de que cuando ingreses y vuelvas a la pagina de inicio sin salir de la cuenta previamente, entonces te aparece la opción PERFIl, si has salido de la cuenta y vuelves a la pagina de inicio, te saldran las opciones: log in y registrarsegit add .!

--- main.py
+++ main.py
@@ -25,19 +25,19 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 # **************************************
 # * ULR Conexion
-MONGO_URL_ATLAS = 'mongodb+srv://mongodb:mongodb@cluster0-l6v7e.mongodb.net/test?retryWrites=true&w=majority'
+MONGO_URL_ATLAS = 'mongodb+srv://matius:mongodb@primercluster-hewbh.mongodb.net/test?retryWrites=true&w=majority'
 
 # * Establecer conexion
 client = MongoClient(MONGO_URL_ATLAS, ssl_cert_reqs=False)
 
 # * Creacion base de datos MongoDB
-db = client['nombre_BD']
+db = client['ProyectoFinal_BD']
 
 # * Creacion coleccion
 collection = db['nombreCollection']
 
 # * Collecion usuarios, por si necesitas un login
-# collectionUsuarios = db['usuarios']
+collectionUsuarios = db['usuarios']
 
 # *******************************************
 
@@ -67,139 +67,142 @@ def home():
     RUTA HOME
     """
 
-    return render_template('home.html')
+    logo = 'logo'
+
+    # print(f'todavía existe la session: ' + session['usuario'])
+
+    if 'usuario' in session:
+
+        print(f'todavía existe la session: ' + session['usuario'])
+
+        return render_template('home.html', usuario_existe=True, logo='logo')
+
+    return render_template('home.html', logo=logo)
 
 # ******************************************
 # * Si tienes SESSION, que cuando vuelva a HOME, toda la sección que tengas se borre, en este
 # * caso es con POST, es decir, que según la página en la que estes, el boton para volver a home.html
 # #* debe tener un FORM de POST.
 # * Por sí lo necesitas
-# @app.route('/home', methods=['GET', 'POST'])
-# def homeDatos():
-
+@app.route('/home', methods=['GET', 'POST'])
+def homeDatos():
     """
     LLEGADA DE DATOS A LA RUTA HOME, ESTO ES PARA PODER LIMPIAR LA SESSION CUANDO VUELVAN A HOME A TRAVEZ DE LA APLICACIÓN.
     """
 
-#     if request.method == 'POST':
+    if request.method == 'POST':
 
-#         session.clear()
+        print('Se ha limpiado la session')
 
-#     if 'usuario' in session:
+        session.clear()
 
-#         return redirect(url_for('nuevaRuta'))
-
-#     return render_template('home.html')
+    return render_template('home.html', usuario_existe=False, logo='logo')
 
 # ************************************************
 
 # * Login por si lo necesitas
 # * Solo le falta la contraseña, simplemente sería agregarsela
-# @app.route('/usuario')
-# def usuario():
-
+@app.route('/usuario')
+def usuario():
     """
     RUTA USUARIO, PARA INGRESAR EL LOGIN. USUARIO Y CONTRAÑESA, O SOLO USUARIO DEPENDIENDO DEL CASO
     """
 
-#     return render_template('usuario.html')
+    return render_template('usuario.html')
 
 # ******************************************
-# @app.route('/usuario', methods=['POST'])
-# def usuariodatos():
-
+@app.route('/usuario', methods=['POST'])
+def usuariodatos():
     """
     LLEGADA DATOS DE USUARIO, CON LOS DATOS DEL LOGIN, PARA PODER INGRESAR A LA APLICACIÓN EN SÍ.
     """
 
-#     # * Lista que almacena al usuario
-#     listaUsuarioCorrecto = []
+    # * Lista que almacena al usuario
+    listaUsuarioCorrecto = []
 
-#     usuario = request.form['usuario']
+    usuario = request.form['usuario']
 
-#     leer_usuario = collectionUsuarios.find({'usuario': f'{usuario}'})
+    leer_usuario = collectionUsuarios.find({'usuario': f'{usuario}'})
 
-#     # print(list(leer_usuario))
+    # print(list(leer_usuario))
 
-#     for i in leer_usuario:
+    for i in leer_usuario:
 
-#         print(i['usuario'])
-#         listaUsuarioCorrecto.append(i['usuario'])
+        print(i['usuario'])
+        listaUsuarioCorrecto.append(i['usuario'])
 
-#     # print(listaUsuarioCorrecto[0])
+    # print(listaUsuarioCorrecto[0])
 
-#     if listaUsuarioCorrecto != []:
+    if listaUsuarioCorrecto != []:
 
-#         if listaUsuarioCorrecto[0] == usuario:
-#             # * iniciar sesion
-#             # * Limpiamos la session cada vez que haga una nueva session.
-#             session.clear()
-#             session['usuario'] = usuario
-#             print('session creada')
+        if listaUsuarioCorrecto[0] == usuario:
+            # * iniciar sesion
+            # * Limpiamos la session cada vez que haga una nueva session.
+            session.clear()
+            session['usuario'] = usuario
+            print('session creada')
 
-#             return redirect(url_for('tipoDados'))
+            return redirect(url_for('perfil'))
 
-#         else:
+        else:
 
-#             return render_template('usuario.html', no_usuario=True)
-#     else:
+            return render_template('usuario.html', no_usuario=True)
+    else:
 
-#         return render_template('usuario.html', no_usuario=True)
+        return render_template('usuario.html', no_usuario=True)
 
-#     return render_template('usuario.html')
+    return render_template('usuario.html')
 
 
 # ******************************************
-# @app.route('/registro')
-# def registro():
-
+@app.route('/registro')
+def registro():
     """
     RUTA REGISTRO, PARA REGISTRARSE EN LA APLICACIÓN
     """
 
-#     return render_template('registro.html')
+    return render_template('registro.html')
 
 
 # ******************************************
-# @app.route('/registro', methods=['GET', 'POST'])
-# def registroDatos():
-
+@app.route('/registro', methods=['GET', 'POST'])
+def registroDatos():
     """
     LLEGADA DE DATOS A LA RUTA REGISTRO. PARA REGISTRAR AL USUARIO EN LA APLICACIÓN
     """
 
-#     if request.method == 'POST':
+    if request.method == 'POST':
 
-#         usuario = request.form['usuario']
+        usuario = request.form['usuario']
 
-#         leer_usuario = collectionUsuarios.find({'usuario': f'{usuario}'})
+        leer_usuario = collectionUsuarios.find({'usuario': f'{usuario}'})
 
-#         # print(list(leer_usuario))
+        # print(list(leer_usuario))
 
-#         # * Si en la BD no esta vacio, entonces el email que se ingreso en el input existe en la BD
-#         if list(leer_usuario) != []:
+        # * Si en la BD no esta vacio, entonces el email que se ingreso en el input existe en la BD
+        if list(leer_usuario) != []:
 
-#             return render_template('registro.html', usuario_existe=True)
+            return render_template('registro.html', usuario_existe=True)
 
-#         collectionUsuarios.insert_one({"usuario": usuario})
+        collectionUsuarios.insert_one({"usuario": usuario})
 
-#         return redirect(url_for('usuario'))
+        return redirect(url_for('usuario'))
 
-#     return render_template('registro.html')
+    return render_template('registro.html')
 
 
 # ******************************************
 
 # * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
 
- @app.route('/perfil')
- def perfil():
+@app.route('/perfil')
+def perfil():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
-    return render_template('nuevaRuta.html')
+    return render_template('perfil.html')
 
 
 # ******************************************
@@ -218,158 +221,158 @@ def perfilDatos():
 
 # * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
 
- @app.route('/reciclar')
- def reciclar():
+@app.route('/reciclar')
+def reciclar():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
-     return render_template('reciclar.html')
+    return render_template('reciclar.html')
 
 
 # ******************************************
 
 
- @app.route('/reciclar', methods=['POST'])
- def reciclarDatos():
+@app.route('/reciclar', methods=['POST'])
+def reciclarDatos():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
- return render_template('reciclar.html')
-
-# ******************************************
-
-# * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
-
- @app.route('/eventos')
- def eventos():
-
-    """
-    RUTA NOMBRERUTA, TE PERMITE
-    """
-    
-
-     return render_template('eventos.html')
-
-
-# ******************************************
-
-
- @app.route('/eventos', methods=['POST'])
- def eventosDatos():
-
-    """
-    RUTA NOMBRERUTA, TE PERMITE
-    """
-
- return render_template('eventos.html')
+    return render_template('reciclar.html')
 
 # ******************************************
 
 # * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
 
- @app.route('/educacion')
- def educacion():
+@app.route('/eventos')
+def eventos():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
-     return render_template('educacion.html')
+
+    return render_template('eventos.html')
 
 
 # ******************************************
 
 
- @app.route('/educacion', methods=['POST'])
- def educacionDatos():
+@app.route('/eventos', methods=['POST'])
+def eventosDatos():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
- return render_template('educacion.html')
-
-# ******************************************
-
-# * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
-
- @app.route('/crearEvento')
- def crearEvento():
-
-    """
-    RUTA NOMBRERUTA, TE PERMITE
-    """
-
-     return render_template('crearEvento.html')
-
-
-# ******************************************
-
-
- @app.route('/crearEvento', methods=['POST'])
- def crearEventoDatos():
-
-    """
-    RUTA NOMBRERUTA, TE PERMITE
-    """
-
- return render_template('crearEvento.html')
+    return render_template('eventos.html')
 
 # ******************************************
 
 # * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
 
- @app.route('/noticias')
- def noticias():
+@app.route('/educacion')
+def educacion():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
-     return render_template('noticias.html')
+    return render_template('educacion.html')
 
 
 # ******************************************
 
 
- @app.route('/noticias', methods=['POST'])
- def noticiasDatos():
+@app.route('/educacion', methods=['POST'])
+def educacionDatos():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
- return render_template('noticias.html')
+    return render_template('educacion.html')
 
 # ******************************************
 
 # * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
 
- @app.route('/denuncia')
- def denuncia():
+@app.route('/crearEvento')
+def crearEvento():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
-     return render_template('denuncia.html')
+    return render_template('crearEvento.html')
 
 
 # ******************************************
 
 
- @app.route('/denuncia', methods=['POST'])
- def denunciaDatos():
+@app.route('/crearEvento', methods=['POST'])
+def crearEventoDatos():
 
     """
     RUTA NOMBRERUTA, TE PERMITE
     """
 
- return render_template('denuncia.html')
+    return render_template('crearEvento.html')
+
+# ******************************************
+
+# * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
+
+@app.route('/noticias')
+def noticias():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+    return render_template('noticias.html')
+
+
+# ******************************************
+
+
+@app.route('/noticias', methods=['POST'])
+def noticiasDatos():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+    return render_template('noticias.html')
+
+# ******************************************
+
+# * Si necesitas más rutas, copia y pega estas dos plantillas de rutas
+
+@app.route('/denuncia')
+def denuncia():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+    return render_template('denuncia.html')
+
+
+# ******************************************
+
+
+@app.route('/denuncia', methods=['POST'])
+def denunciaDatos():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+    return render_template('denuncia.html')
 
 # ******************************************
 
